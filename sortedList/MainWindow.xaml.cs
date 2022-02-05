22a831b4dc3716432d4271bb7f9bbbf35e565279
jomjamdkk <jomjamdkk@gmail.com>
2022-02-05 22:01:07 +0700

    Do Homework

--- sortedList/MainWindow.xaml.cs
+++ sortedList/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,28 @@ namespace sortedList
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+        SortedList list = new SortedList();
+     
+        private void AddBtn_click(object sender, RoutedEventArgs e)
+        {
+            list.Add(Keytxt.Text,Valuetxt.Text);
+        }
+
+        private void ShowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ICollection collection = list.Keys;
+            string allValue = "All Value :\n";
+            foreach (string key in collection)
+            {
+                allValue = allValue + "   " + list[key] + "\n";
+            }
+            MessageBox.Show(allValue);
         }
     }
 }
